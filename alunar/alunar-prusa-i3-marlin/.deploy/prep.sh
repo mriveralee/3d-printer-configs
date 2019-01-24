e5d4f3e5e48ed696b74542b918f7405a9be374bb
Michael Rivera <mrivera.lee@gmail.com>
2019-01-24 13:07:15 -0500

    add alunar printer firmware

---
+++ alunar/alunar-prusa-i3-marlin/.deploy/prep.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# library-manager --name="LiquidCrystal" --version="latest";
+# library-manager --name="LiquidCrystal_I2C" --version="latest";
+exit 0;
---
+++ alunar/alunar-prusa-i3-marlin/.deploy/test.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+set -e;
+
+__error() {
+	RED='\033[0;31m';
+	NC='\033[0m';
+	DT=$(date '+%F %T');
+	(>&2 echo -e "${RED}[$DT]\t$(basename $0)\t${1:-"UNKNOWN ERROR"}${NC}");
+	exit 100;
+}
+
+__warning() {
+	YELLOW='\033[0;33m';
+	NC='\033[0m';
+	DT=$(date '+%F %T');
+	(>&2 echo -e "${YELLOW}[$DT]\t$(basename $0)\t${1:-"UNKNOWN ERROR"}${NC}");
+}
+
+kill_mega() {
+	docker kill "$1" ||  docker rm "$1";
+}
+
+for i in "$@"; do
+	case $i in
+		-h=*|--hex=*)
+			opt_hex="${i#*=}";
+			shift # past argument=value
+		;;
+		*)
+			(>&2 echo "Invalid argument: ${i}");
+			exit 1;
+		;;
+	esac
+done
+
+[[ -z "${opt_hex// }" ]] && (>&2 echo "missing file to flash") && exit 1;
+[[ ! -f "${opt_hex}" ]] && (>&2 echo "hex file (${opt_hex}) does not exist.") && exit 1;
+
+
+PULL_REPOSITORY="${DOCKER_REGISTRY:-"docker.artifactory.bit13.local"}";
+
+mkdir -p /tmp/simduino;
+
+uart=$(docker run -d \
+	--privileged \
+	-v /tmp/simduino:/dev/pts \
+	-t "${PULL_REPOSITORY}/camalot/mega2560simulator:latest");
+	# --device=/dev/ttyACM0:/dev/pts/0 \
+
+sleep 5s;
+wait_count=0;
+
+echo "";
+echo "Waiting for device to become ready.";
+while [ $wait_count -lt 60 ]; do
+	[ -c /tmp/simduino/0 ] && wait_count=99;
+	sleep 1s;
+	wait_count=$[$wait_count+1];
+	echo -n '.';
+done
+echo "";
+
+ls -lFA /tmp/simduino;
+
+[ $wait_count -ne 99 ] && kill_mega "$uart" && __warning "Timeout exceeded waiting for device to become ready." && exit 255;
+
+if [[ ! -L /tmp/simduino/0 ]]; then
+	kill_mega "$uart";
+	__warning "Device /tmp/simduino/0 not found. Skipping avrdude tests.";
+	exit 255;
+fi
+
+
+avrdude -p m2560 -c arduino -P /tmp/simduino/0 -C /usr/local/etc/avrdude.conf -D -U "flash:w:${opt_hex}:i"
+# avrdude -p m2560 -c avrispmkII -P /tmp/simduino/0 -C /usr/local/etc/avrdude.conf -D -U flash:v:${opt_hex}:i
+
+marlin_data=$(python /bin/marlin-identify.py -d '/tmp/simduino/0' -s 250000);
+
+[[ ! $marlin_data =~ echo:Marlin ]] && __error "Unable to located Marlin version";
+[[ ! $marlin_data =~ FIRMWARE_NAME:Marlin ]] && __error "Unable to located Marlin FIRMWARE_NAME";
+
+kill_mega "$uart";
